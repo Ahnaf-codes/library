36eae7edf7ece56e3ca196927b721914ba7d0b1d
Ahnaf <ahnafanis0@gmail.com>
2023-09-12 17:27:21 +0600

    Add Book Form

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-"use strict"
+"use strict";
+let library = [];
+const addBookBtn = document.getElementById("add-book-btn");
+const modalBg = document.getElementById("modal-bg");
+const bookFormContainer = document.getElementById("book-form-container");
+const closeModal = document.getElementById("close");
+
+
+addBookBtn.addEventListener("click", () => {
+    modalBg.style.visibility = "visible";
+    bookFormContainer.classList.add("open-modal");
+});
+
+closeModal.addEventListener("click", () => {
+    modalBg.style.visibility = "hidden";
+    bookFormContainer.classList.remove("open-modal");
+});
