8ad4021247ce36f8fb51284beebc256eab3c1c15
Ahnaf <ahnafanis0@gmail.com>
2023-09-14 21:36:23 +0600

    form submission

--- index.js
+++ index.js
@@ -6,21 +6,37 @@ const bookFormContainer = document.getElementById("book-form-container");
 const form = document.getElementById("book-form");
 const closeModal = document.getElementById("close");
 
-
-addBookBtn.addEventListener("click", () => {
-    modalBg.style.visibility = "visible";
-    bookFormContainer.classList.add("open-modal");
-});
-
-closeModal.addEventListener("click", () => {
-    modalBg.style.visibility = "hidden";
-    bookFormContainer.classList.remove("open-modal");
-    form.reset();
-});
-
 function book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.status = status;
 }
+
+function close() {
+    modalBg.style.visibility = "hidden";
+    bookFormContainer.classList.remove("open-modal");
+    form.reset();
+};
+
+addBookBtn.addEventListener("click", () => {
+    modalBg.style.visibility = "visible";
+    bookFormContainer.classList.add("open-modal");
+});
+
+closeModal.addEventListener("click", close);
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const title = document.getElementById("book-title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const status = document.getElementById("status").value;
+    const newBook = new book(title, author, pages, status);
+    library.push(newBook);
+    console.log(newBook);
+    close();
+});
+
+
+
