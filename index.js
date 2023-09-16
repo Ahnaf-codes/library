d02de7f24205ed0bebb726e854b7b0628a60417f
Ahnaf <ahnafanis0@gmail.com>
2023-09-16 16:47:58 +0600

    Display book functionality

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const modalBg = document.getElementById("modal-bg");
 const bookFormContainer = document.getElementById("book-form-container");
 const form = document.getElementById("book-form");
 const closeModal = document.getElementById("close");
+const bookGrid = document.getElementById("book-grid");
 
 function book(title, author, pages, status) {
     this.title = title;
@@ -18,6 +19,17 @@ function close() {
     bookFormContainer.classList.remove("open-modal");
     form.reset();
 };
+
+function displayBook(book) {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    bookCard.classList.add("grid");
+    bookCard.innerHTML = `<h3 class="book-title">${book.title}</h3>
+    <p class="book-author">${book.author}</p>
+    <p class="book-pages">${book.pages}</p>
+    <p class="book-status">${book.status}</p>`;
+    bookGrid.appendChild(bookCard);
+}
 
 addBookBtn.addEventListener("click", () => {
     modalBg.style.visibility = "visible";
@@ -34,6 +46,7 @@ form.addEventListener("submit", (event) => {
     const status = document.getElementById("status").value;
     const newBook = new book(title, author, pages, status);
     library.push(newBook);
+    displayBook(newBook);
     console.log(newBook);
     close();
 });
